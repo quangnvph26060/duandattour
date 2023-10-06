feb8b570a81d3b276b29a58ebc42d268e7f847ae
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2023-10-06 11:21:16 +0700

    api_phuong_tien

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//api phương tiện
+Route::prefix('phuongtien')->group(function () {
+    Route::get('/', [\App\Http\Controllers\api\ApiLoaiPhuongTienController::class, 'index']); // lấy ra danh sách
+    Route::post('/', [\App\Http\Controllers\api\ApiLoaiPhuongTienController::class, 'store']); //  thêm 1 phương tiện mới
+    Route::get('/{id}', [\App\Http\Controllers\api\ApiLoaiPhuongTienController::class, 'show']); // lấy ra  id muốn sửa
+    Route::put('/{id}', [\App\Http\Controllers\api\ApiLoaiPhuongTienController::class, 'update']); // sủa theo id
+    Route::delete('/{id}', [\App\Http\Controllers\api\ApiLoaiPhuongTienController::class, 'destroy']); // xóa theo id
+});
