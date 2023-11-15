017fa83a0b3e211e4124c444bed0f3955584ceaf
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2023-11-15 22:56:11 +0700

    update code

--- back_end/app/Http/Controllers/api/ApiDatTourController.php
+++ back_end/app/Http/Controllers/api/ApiDatTourController.php
@@ -74,35 +74,35 @@ class ApiDatTourController extends Controller
     }
 
 
-    // public function demo()
-    // {
-    //     $currentDate = Carbon::now()->toDateString(); // Lấy ngày hiện tại
-    //     $tours = DatTour::all(); // Lấy danh sách các tour
+    public function demo()
+    {
+        $currentDate = Carbon::now()->toDateString(); // Lấy ngày hiện tại
+        $tours = DatTour::all(); // Lấy danh sách các tour
 
-    //     foreach ($tours as $tour) {
-    //         $tourdetail = DB::table('tour')->where('id', $tour->id_tour)->first();
+        foreach ($tours as $tour) {
+            $tourdetail = DB::table('tour')->where('id', $tour->id_tour)->first();
 
-    //         // dd($tourdetail->soluong); // số lượng khách 
-    //         $expirationDate = $tour->ngay_het_han;
-    //         echo $expirationDate < $currentDate;
-    //         if ($expirationDate < $currentDate && $tour->trang_thai == 0) {
-    //             //  $tourdetail->update(['soluong'=>$tourdetail->soluong + $tour->so_luong_khach]);
-    //             $affected = DB::table('tour')
-    //                 ->where('id', $tour->id_tour)
-    //                 ->update(['soluong' =>  DB::raw('soluong + ' . $tour->so_luong_khach)]);
+            // dd($tourdetail->soluong); // số lượng khách 
+            $expirationDate = $tour->ngay_het_han;
+            echo $expirationDate < $currentDate;
+            if ($expirationDate < $currentDate && $tour->trang_thai == 0) {
+                //  $tourdetail->update(['soluong'=>$tourdetail->soluong + $tour->so_luong_khach]);
+                $affected = DB::table('tour')
+                    ->where('id', $tour->id_tour)
+                    ->update(['soluong' =>  DB::raw('soluong + ' . $tour->so_luong_khach)]);
 
-    //             if ($tour->ThanhToan) {
-    //                 $tour->ThanhToan->delete();
-    //             }
+                if ($tour->ThanhToan) {
+                    $tour->ThanhToan->delete();
+                }
 
 
-    //             // Xóa tour
-    //             $tour->delete();
-    //         }
-    //     }
+                // Xóa tour
+                $tour->delete();
+            }
+        }
 
-    //     return 'Xóa các tour hết hạn thành công';
-    // }
+        return 'Xóa các tour hết hạn thành công';
+    }
 
     public function getListBookingTour()
     {
