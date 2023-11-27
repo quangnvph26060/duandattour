6c4352a15fe7edc64ea88e60c63b387e3c694615
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2023-11-27 14:45:15 +0700

    update db

--- back_end/database/migrations/2023_11_26_113037_create_hdv_tour_table.php
+++ back_end/database/migrations/2023_11_26_113037_create_hdv_tour_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('tour_id');
             $table->date('start_date');
             $table->date('end_date');
+            $table->tinyInteger('status')->default(0);
             $table->timestamps();
             
             $table->foreign('hdv_id')->references('id')->on('users')->onDelete('cascade');
