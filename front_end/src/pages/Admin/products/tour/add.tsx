d874cae963d27d40640c0240a120b2475ff4df8a
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2023-12-05 21:39:59 +0700

    update code

--- front_end/src/pages/Admin/products/tour/add.tsx
+++ front_end/src/pages/Admin/products/tour/add.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Button, Input, DatePicker, Select, InputNumber } from 'antd';
+import { Form, Button, Input, DatePicker, Select, InputNumber, message } from 'antd';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 import { useGetLoaiTourQuery } from "../../../../api/LoaiTourApi";
@@ -13,7 +13,6 @@ import { Upload } from 'antd';
 import { UploadOutlined } from "@ant-design/icons";
 // import ImgCrop from 'antd-img-crop';
 import { PlusOutlined } from '@ant-design/icons';
-import axios from 'axios';
 
 
 const { Option } = Select;
@@ -46,7 +45,14 @@ const AdminTourAdd: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState(null);
 
+  const [messageApi, contextHolder] = message.useMessage();
 
+  const success = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'Thêm thành công',
+    });
+  };
 
   const onFinish = (values: FieldType) => {
     const formData = new FormData();
@@ -60,6 +66,8 @@ const AdminTourAdd: React.FC = () => {
     formData.append("diem_di", values.diem_di);
     formData.append("diem_den", values.diem_den);
     formData.append("diem_khoi_hanh", values.diem_khoi_hanh);
+    formData.append("lich_khoi_hanh", values.lich_khoi_hanh);
+    formData.append("ngay_ket_thuc", values.ngay_ket_thuc);
     formData.append("gia_nguoilon", values.gia_nguoilon);
     formData.append("gia_treem", values.gia_treem);
     formData.append("soluong", values.soluong);
@@ -67,12 +75,17 @@ const AdminTourAdd: React.FC = () => {
     values.mo_ta = editorData;
     formData.append("mo_ta", values.mo_ta);
 
-    addTour(formData) // Sử dụng formData chứa hình ảnh
-      .unwrap()
-      .then(() => navigate("/admin/tour"))
-      .catch((error) => {
-        setErrors(error.data.message);
-      });
+    addTour(formData)
+    .unwrap()
+    .then(() => {
+      success();
+      setTimeout(() => {
+        navigate('/admin/tour');
+      }, 2000); // Trì hoãn chuyển hướng sau 1 giây (1000ms)
+    })
+    .catch((error) => {
+      setErrors(error.data.message);
+    });
   };
 
   const { data: loaitourdata } = useGetLoaiTourQuery();
@@ -117,80 +130,15 @@ const AdminTourAdd: React.FC = () => {
     return current && current < currentDate.setHours(0, 0, 0, 0);
   };
 
-
-  // test chọn ngày đi và kết thúc 
-
-  const [HDVArrary, setHDVArrary] = useState([]);
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const handleStartDateChange = (date: any, dateString: any) => {
-    setStartDate(dateString);
-  };
-
-  const handleEndDateChange = (date: any, dateString: any) => {
-    setEndDate(dateString);
-  };
-  useEffect(() => {
-    if (startDate && endDate) {
-      fetchData();
-    }
-  }, [startDate, endDate]);
-  const fetchData = () => {
-    axios.post('http://127.0.0.1:8000/api/admin/hdvtour', { start_date: startDate, end_date: endDate })
-      .then(response => {
-        let hdvDate = response.data;
-        axios.get('http://127.0.0.1:8000/api/admin/user')
-          .then(response => {
-            let ShowUserAll = response.data;
-            const matchingRecords = [];
-            ShowUserAll.data.map((item) => {
-              hdvDate.map((itemhdv) => {
-                // kiểm tra nếu thành công 
-                if (item.id === itemhdv.hdv_id) {
-                  const isDuplicate = matchingRecords.some((record) => record.id === item.id);
-                  if (!isDuplicate) {
-                    matchingRecords.push(item);
-                  }
-                }
-              });
-
-
-            });
-
-            setHDVArrary(matchingRecords);
-            console.log(HDVArrary);
-            
-          })
-          .catch(error => {
-            // Xử lý lỗi
-            console.error(error);
-          });
-
-      })
-      .catch(error => {
-        // Xử lý lỗi
-        console.error(error);
-      });
-  };
-  // const handleSelectChange = (selectedRecordId) => {
-  //   // Xử lý các thao tác sau khi lựa chọn được thay đổi, sử dụng giá trị selectedRecordId
-  //   // Ví dụ: Gọi hàm xử lý atnd với selectedRecordId
-  //   yourATNDFunction(selectedRecordId);
-  // };
   return (
+  
     <div className="container">
+    
       <header className="mb-4">
         <h2 className="font-bold text-2xl">Tạo mới tour</h2>
       </header>
-      {/* <Select >
-        
-        { HDVArrary.length>0&&HDVArray.map((record) => (
-          <Option key={record.id} value={record.id}>
-            
-            {record.id} - {record.name}
-          </Option>
-        ))}
-      </Select> */}
+      <div>
+        {contextHolder}
       <Form
         className="tour-form"
         name="basic"
@@ -200,17 +148,27 @@ const AdminTourAdd: React.FC = () => {
         onFinish={onFinish}
         autoComplete="off"
       >
-        <Form.Item
-          label="Tên tour"
-          name="ten_tour"
-          rules={[
-            { required: true, message: 'Vui lòng nhập tên tour!' },
-            { min: 3, message: 'Tên tour ít nhất 3 ký tự' },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
+        <div className='flex w-[1000px] container mx-auto'>
+        <div className='w-1/2'>
+        <Form.Item className='w-full'
+  label="Tên tour"
+  name="ten_tour"
+  rules={[
+    { required: true, message: 'Vui lòng nhập tên tour!' },
+    { min: 3, message: 'Tên tour ít nhất 3 ký tự' },
+    {
+      validator(_, value) {
+        if (value && value.trim() === "") {
+          return Promise.reject('Tên tour không thể chỉ chứa khoảng trắng');
+        }
+        return Promise.resolve();
+      },
+    },
+  ]}
+>
+  <Input />
+</Form.Item>
+        <Form.Item className='w-full'
           label="Hình ảnh"
           name="hinh"
           rules={[{ required: true, message: "Vui lòng chọn ảnh" }]}
@@ -227,7 +185,7 @@ const AdminTourAdd: React.FC = () => {
         </Form.Item>
 
 
-        <Form.Item
+        <Form.Item className='w-full'
           label="Điểm khởi hành"
           name="diem_khoi_hanh"
           rules={[{ required: true, message: 'Vui lòng chọn điểm khởi hành!' }]}
@@ -241,7 +199,7 @@ const AdminTourAdd: React.FC = () => {
             ))}
           </Select>
         </Form.Item>
-        <Form.Item
+        <Form.Item className='w-full'
           label="Điểm đi"
           name="diem_di"
           rules={[{ required: true, message: 'Vui lòng chọn điểm đi!' }]}
@@ -255,7 +213,7 @@ const AdminTourAdd: React.FC = () => {
             ))}
           </Select>
         </Form.Item>
-        <Form.Item
+        <Form.Item className='w-full'
           label="Điểm đến"
           name="diem_den"
           rules={[{ required: true, message: 'Vui lòng chọn điểm đến!' }]}
@@ -271,24 +229,21 @@ const AdminTourAdd: React.FC = () => {
 
 
         </Form.Item>
-        <Form.Item
+        <Form.Item className='w-full'
           label="Lịch khởi hành"
           name="lich_khoi_hanh"
           rules={[{ required: true, message: 'Vui lòng nhập lịch khởi hành!' }]}
         >
-          <DatePicker style={{ width: '100%' }} disabledDate={disabledDate}
-
-            onChange={handleStartDateChange} />
+          <DatePicker style={{ width: '100%' }} disabledDate={disabledDate} />
         </Form.Item>
-        <Form.Item
+        <Form.Item className='w-full'
           label="Ngày kết thúc"
           name="ngay_ket_thuc"
           rules={[{ required: true, message: 'Vui lòng nhập thời gian!' }]}
         >
-          <DatePicker style={{ width: '100%' }} disabledDate={disabledDate}
-            onChange={handleEndDateChange} />
+          <DatePicker style={{ width: '100%' }} disabledDate={disabledDate} />
         </Form.Item>
-        <Form.Item
+        <Form.Item 
           label="Giá Người lớn"
           name="gia_nguoilon"
           rules={[
@@ -322,6 +277,10 @@ const AdminTourAdd: React.FC = () => {
         >
           <InputNumber min={1000000} parser={value => value.replace(/\$\s?|(,*)/g, '')} />
         </Form.Item>
+        </div>
+        <div>
+
+        
         <Form.Item
           label="Mô Tả"
           name="mo_ta"
@@ -359,9 +318,11 @@ const AdminTourAdd: React.FC = () => {
             ))}
           </Select>
         </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
+        </div>
+        </div>
+       
+        <Form.Item className='' wrapperCol={{ offset: 8, span: 16 }}>
+          <Button className='bg-blue-500' type="primary" htmlType="submit">
             Thêm
           </Button>
           <Button
@@ -373,6 +334,7 @@ const AdminTourAdd: React.FC = () => {
           </Button>
         </Form.Item>
       </Form>
+      </div>
     </div>
   );
 };
