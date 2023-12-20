423e1c1c209c717d540c8d28b66eccb3557adb23
HuynhPhamVan <128672412+HuynhPhamVan@users.noreply.github.com>
2023-12-20 20:10:31 +0700

    tim kiem danh muc va bai viet

--- front_end/src/pages/Admin/Post/bai_viet/index.tsx
+++ front_end/src/pages/Admin/Post/bai_viet/index.tsx
@@ -1,88 +1,121 @@
-import React from "react";
-import { Table, Button, Skeleton, Popconfirm } from "antd";
-import { Link } from "react-router-dom";
-import { AiOutlinePlus } from "react-icons/ai";
+import React, { useState } from 'react';
+import { Table, Button, Skeleton, Popconfirm, Input } from 'antd';
+import { Link } from 'react-router-dom';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const Admin_baiviet = () => {
-    const dataSource = [
-        {
-            key: 1,
-            ten_bai_viet: "Bài viết cương",
-            image: "image_url_1.jpghttps://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/399083074_666738505589224_5550605208199897590_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Z6q--I4u1U4AX-mpVqv&_nc_ht=scontent.fhan14-3.fna&oh=00_AfAhGnPQEVEVOXt_CGOJHdx5C6cGjN-dFNyeuyhTrrPEMA&oe=6566DDAE", // You can use the image URL or any other representation you need
-            mo_ta: "Mô tả bài viết 1",
-        },
-        {
-            key: 2,
-            ten_bai_viet: "Bài viết thái",
-            image: "https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/399083074_666738505589224_5550605208199897590_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Z6q--I4u1U4AX-mpVqv&_nc_ht=scontent.fhan14-3.fna&oh=00_AfAhGnPQEVEVOXt_CGOJHdx5C6cGjN-dFNyeuyhTrrPEMA&oe=6566DDAE",
-            mo_ta: "Mô tả bài viết 2",
-        },
-        // Add more data objects as needed
-    ];
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
 
-    const columns = [
-        {
-            title: "ID bài viết",
-            dataIndex: "key",
-            key: "key",
-        },
-        {
-            title: "Tên bài viết",
-            dataIndex: "ten_bai_viet",
-            key: "ten_bai_viet",
-        },
-        {
-            title: "Ảnh",
-            dataIndex: "image",
-            key: "image",
-            render: (image) => (
-                <img src={image} alt="Bài viết" style={{ maxWidth: "100px", maxHeight: "100px" }} />
-            ),
-        },
-        {
-            title: "Mô tả",
-            dataIndex: "mo_ta",
-            key: "mo_ta",
-        },
-        {
-            title: "Action",
-            render: ({ key: id }) => {
-                return (
-                    <>
-                        <div className="flex space-x-2">
-                            <Popconfirm
-                                title="Bạn có muốn xóa?"
-                                onConfirm={() => confirm(id)}
-                                okText="Yes"
-                                cancelText="No"
-                            >
-                                <Button type="primary" danger>
-                                    Xóa
-                                </Button>
-                            </Popconfirm>
-                            {/* Add other action buttons or links here */}
-                        </div>
-                    </>
-                );
-            },
-        },
-    ];
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
-    return (
-        <div>
-            <header className="mb-4 flex justify-between items-center">
-                <h2 className="font-bold text-2xl">Quản lý  bài viết</h2>
+  const handleSearch = () => {
+    if (searchValue) {
+      const filteredData = dataSource.filter((item) =>
+        item.ten_bai_viet.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredDataSource(filteredData);
+    } else {
+      setFilteredDataSource([]);
+    }
+  };
+
+  const confirm = (id) => {
+    // Handle delete action here
+  };
+
+  const dataSource = [
+    {
+      key: 1,
+      ten_bai_viet: 'Bài viết cương',
+      image: 'image_url_1.jpg',
+      mo_ta: 'Mô tả bài viết 1',
+    },
+    {
+      key: 2,
+      ten_bai_viet: 'Bài viết thái',
+      image: 'image_url_2.jpg',
+      mo_ta: 'Mô tả bài viết 2',
+    },
+    // Add more data objects as needed
+  ];
+
+  const baivietArray = filteredDataSource.length > 0 ? filteredDataSource : dataSource;
+
+  const columns = [
+    {
+      title: 'ID bài viết',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      title: 'Tên bài viết',
+      dataIndex: 'ten_bai_viet',
+      key: 'ten_bai_viet',
+    },
+    {
+      title: 'Ảnh',
+      dataIndex: 'image',
+      key: 'image',
+      render: (image) => (
+        <img src={image} alt="Bài viết" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+      ),
+    },
+    {
+      title: 'Mô tả',
+      dataIndex: 'mo_ta',
+      key: 'mo_ta',
+    },
+    {
+      title: 'Action',
+      render: ({ key: id }) => {
+        return (
+          <>
+            <div className="flex space-x-2">
+              <Popconfirm
+                title="Bạn có muốn xóa?"
+                onConfirm={() => confirm(id)}
+                okText="Yes"
+                cancelText="No"
+              >
                 <Button type="primary" danger>
-                    <Link to="/admin/tour/loai_tour/add" className="flex items-center space-x-2">
-                        <AiOutlinePlus />
-                        Tạo mới bài viết
-                    </Link>
+                  Xóa
                 </Button>
-            </header>
-            {/* Add your table component here */}
-            <Table dataSource={dataSource} columns={columns} />
-        </div>
-    );
+              </Popconfirm>
+              {/* Add other action buttons or links here */}
+            </div>
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <header className="mb-4 flex justify-between items-center">
+        <h2 className="font-bold text-2xl">Quản lý  bài viết</h2>
+        <Button type="primary" danger>
+          <Link to="/admin/tour/loai_tour/add" className="flex items-center space-x-2">
+            <AiOutlinePlus />
+            Tạo mới bài viết
+          </Link>
+        </Button>
+      </header>
+      <div className="flex items-center space-x-2 mb-4">
+        <Input
+          placeholder="Tìm kiếm bài viết"
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+        <Button style={{backgroundColor:'blue'}} type="primary" onClick={handleSearch}>
+          Tìm kiếm
+        </Button>
+      </div>
+      <Table dataSource={baivietArray} columns={columns} />
+    </div>
+  );
 };
 
 export default Admin_baiviet;
--- front_end/src/pages/Admin/Post/danh_muc/index.tsx
+++ front_end/src/pages/Admin/Post/danh_muc/index.tsx
@@ -1,103 +1,112 @@
-type Props = {};
+import React, { useState } from 'react';
+import { Table, Button, Input } from 'antd';
+import { Link } from 'react-router-dom';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { Popconfirm } from 'antd';
+const Admin_Danhmuc_baiviet = (props) => {
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
 
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
-import { useEffect } from "react";
-import { Table, Button, Skeleton, Popconfirm, Alert } from "antd";
-import { Link } from "react-router-dom";
-import useNavigate from "react-router-dom";
-import { AiOutlinePlus } from "react-icons/ai";
+  const handleSearch = () => {
+    if (searchValue) {
+      const filteredData = dataSource.filter((item) =>
+        item.ten_danh_muc.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredDataSource(filteredData);
+    } else {
+      setFilteredDataSource([]);
+    }
+  };
 
-import { ILoaiTour } from "../../../../interface/loaiTour";
-const Admin_Danhmuc_baiviet = (props: Props) => {
-    const dataSource = [
-        {
-            key: 1,
-            ten_danh_muc: "Danh mục 1",
-        },
-        {
-            key: 2,
-            ten_danh_muc: "Danh mục 2",
-        },
-        // Add more data objects as needed
-    ];
-    //cương đb
-    // const { data: tourdata, error, isLoading } =  //thay bang api getdanhmuc useGetLoaiTourQuery();
-    // const [removeProduct, { isLoading: isRemoveLoading, isSuccess: isRemoveSuccess }] =
-    //     useRemoveLoaiTourMutation();
-    // const confirm = (id: any) => {
-    //     removeProduct(id);
-    // };
-    // // const navigate = useNavigate();
-    // const tourArray = tourdata?.data || [];
+  const confirm = (id) => {
+    // Handle delete action here
+  };
 
-    // const dataSource = tourArray.map(({ id, image, ten_loai_tour }: ILoaiTour) => ({
-    //     key: id,
-    //     image,
-    //     ten_loai_tour
-    // }));
+  const dataSource = [
+    {
+      key: 1,
+      ten_danh_muc: 'Danh Mục Giải Trí',
+    },
+    {
+      key: 2,
+      ten_danh_muc: 'Danh Mục Tin Tức',
+    },
+    {
+        key: 3,
+        ten_danh_muc: 'Danh Mục Khuyến Mãi',
+      },
+      {
+        key: 4,
+        ten_danh_muc: 'Danh Mục Biến Động',
+      }
+    // Add more data objects as needed
+  ];
 
-   
+  const danhMucArray = filteredDataSource.length > 0 ? filteredDataSource : dataSource;
 
-    const columns = [
-        {
-            title: "ID loại bài việt",
-            dataIndex: "key",
-            key: "key",
-        },
-       
-        {
-            title: "Tên danh mục",
-            dataIndex: "ten_danh_muc",
-            key: "ten_danh_muc",
-        },
-        {
-            title: "Action",
-            render: ({ key: id }: any) => {
-                return (
-                    <>
-                        {localStorage.getItem("role") == 'admin' ? <div className="flex space-x-2">
-                            <Popconfirm
-                                title="Bạn có muốn xóa?"
-                                onConfirm={() => confirm(id)}
-                                okText="Yes" className="text-black"
-                                cancelText="No"
-                            >
-                                <Button type="primary" danger>
-                                    Xóa
-                                </Button>
-                            </Popconfirm>
-
-                            {/* <Button type="primary" danger>
-                                <Link to={`/admin/tour/loai_tour/edit/${id}`}>Sửa</Link>
-                            </Button> */}
-                        </div> : ""}
-                    </>
-                );
-            },
-        },
-    ];
-    // useEffect(()=>{
-    //     if(isRemoveSuccess){
-    //         // navigator("/admin/tour/loai_tour")
-    //         // Navigator
-    //         useNavigate("/admin/tour/loai_tour");
-    //     }
-    // }, [])
-    return (
-        <div>
-            <header className="mb-4 flex justify-between items-center">
-                <h2 className="font-bold text-2xl">Quản lý danh mục bài viết</h2>
+  const columns = [
+    {
+      title: 'ID loại bài viết',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      title: 'Tên danh mục',
+      dataIndex: 'ten_danh_muc',
+      key: 'ten_danh_muc',
+    },
+    {
+      title: 'Action',
+      render: ({ key: id }) => {
+        return (
+          <>
+            <div className="flex space-x-2">
+              <Popconfirm
+                title="Bạn có muốn xóa?"
+                onConfirm={() => confirm(id)}
+                okText="Yes"
+                cancelText="No"
+              >
                 <Button type="primary" danger>
-                    <Link to="/admin/post/add_danhmuc" className="flex items-center space-x-2">
-                        <AiOutlinePlus />
-                        Tạo mới danh mục bài viết 
-                    </Link>
+                  Xóa
                 </Button>
-            </header>
-            {/* {isRemoveSuccess && <Alert message="Success Text" type="success" />} */}
-            { <Table dataSource={dataSource} columns={columns} />}
-        </div>
-    );
+              </Popconfirm>
+              {/* Add other action buttons or links here */}
+            </div>
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <header className="mb-4 flex justify-between items-center">
+        <h2 className="font-bold text-2xl">Quản lý danh mục bài viết</h2>
+        <Button type="primary" danger>
+          <Link to="/admin/post/add_danhmuc" className="flex items-center space-x-2">
+            <AiOutlinePlus />
+            Tạo mới danh mục bài viết
+          </Link>
+        </Button>
+      </header>
+      <div className="flex items-center space-x-2 mb-4">
+        <Input
+          placeholder="Tìm kiếm danh mục"
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+        <Button style={{backgroundColor:"blue"}} type="primary" onClick={handleSearch}>
+          Tìm kiếm
+        </Button>
+      </div>
+      <Table dataSource={danhMucArray} columns={columns} />
+    </div>
+  );
 };
 
 export default Admin_Danhmuc_baiviet;
