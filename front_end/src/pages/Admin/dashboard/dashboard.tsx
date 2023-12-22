f402b1b6c92c78add28707bf29bdf67311817fee
thaihdph27014 <hoangducthai2301@gmail.com>
2023-12-22 17:59:09 +0700

    color bar

--- front_end/src/pages/Admin/dashboard/dashboard.tsx
+++ front_end/src/pages/Admin/dashboard/dashboard.tsx
@@ -19,26 +19,17 @@ const style: React.CSSProperties = {
   height: "90px",
 };
 const Dashboard = () => {
-  const dataBar = [
-    { name: "Tháng 1", react: 32 },
-    { name: "Tháng 2", react: 42 },
-    { name: "Tháng 3", react: 51 },
-    { name: "Tháng 4", react: 60 },
-    { name: "Tháng 5", react: 51 },
-    { name: "Tháng 6", react: 95 },
-    { name: "Tháng 7", react: 32 },
-    { name: "Tháng 8", react: 42 },
-    { name: "Tháng 9", react: 51 },
-    { name: "Tháng 10", react: 60 },
-    { name: "Tháng 11", react: 51 },
-    { name: "Tháng 12", react: 950 },
-  ];
 
-  // const dataPie = [
-  //   { name: "React", value: 75 },
-  //   { name: "Angular", value: 50 },
-  // ];
-  const colors = ["red", "blue"]; // Mảng các màu cho từng loại dữ liệu
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  const monthColors = Array.from({ length: 12 }, getRandomColor);
   const [statistical, setStatistical] = useState([]);
   const Statistical = () => {
     axios
@@ -83,17 +74,20 @@ const Dashboard = () => {
     ];
 
     axios
-      .get( `http://127.0.0.1:8000/api/admin/statistical/columnChart/${selectedYear}`)
+      .get(
+        `http://127.0.0.1:8000/api/admin/statistical/columnChart/${selectedYear}`
+      )
       .then((response) => {
         const newData = dataBar.map((item, index) => ({
           name: item.name,
           doanh_thu: response.data.doanhthu[index],
+          fill: monthColors[index]  
         }));
         setColumnChart(newData);
 
         console.log(newData);
       });
-  }
+  };
   useEffect(() => {
     ColumnChart(selectedYear);
   }, []);
@@ -106,7 +100,7 @@ const Dashboard = () => {
 
   const handleFilterData = () => {
     console.log(selectedYear);
-    ColumnChart(selectedYear) // Lấy giá trị năm đã chọn khi người dùng nhấp vào nút "Lọc dữ liệu"
+    ColumnChart(selectedYear); // Lấy giá trị năm đã chọn khi người dùng nhấp vào nút "Lọc dữ liệu"
     // Thực hiện xử lý dữ liệu dựa trên năm đã chọn
   };
 
@@ -266,16 +260,22 @@ const Dashboard = () => {
 
       <div className="chart-container">
         <div className="chart-item chart-item-left">
-        <h3>Biểu đồ doanh thu của năm {selectedYear ? selectedYear : "2023"}</h3>
+          <h3>
+            Biểu đồ doanh thu của năm {selectedYear ? selectedYear : "2023"}
+          </h3>
           <br />
           <BarChart width={1000} height={400} data={columnChart}>
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip formatter={(value) => `${value} VNĐ`} />
-            <Legend />
-            <Bar dataKey="doanh_thu" fill="#2196F3" />
-          </BarChart>
+  <CartesianGrid stroke="#ccc" />
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Tooltip formatter={(value) => `${value} VNĐ`} />
+  <Legend />
+  <Bar dataKey="doanh_thu" name="Doanh thu" fill={getRandomColor()} />
+
+  {/* Thêm các cột khác tương tự */}
+</BarChart>
+
+
         </div>
         {/* <div className="chart-item chart-item-right">
           <h3>Biểu đồ tròn</h3>
