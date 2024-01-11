3d3f3deab0bec38fe3afcab3ac579a15cb11da7f
hieunguyen29 <105211474+hieunguyen29@users.noreply.github.com>
2024-01-11 18:42:20 +0700

    update-logo

---
+++ front_end/src/pages/Admin/products/Logo/add.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Add_logo = () => {
+  return (
+    <div>_logo</div>
+  )
+}
+
+export default Add_logo
---
+++ front_end/src/pages/Admin/products/Logo/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Logo
